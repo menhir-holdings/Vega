f68b45511c32d6a100dacf81f795955d572c7476
Phil Ledoit <pledoit@bu.edu>
2026-09-14 13:18:33 +0800

    Give the opening still a full viewport crop instead of a letterboxed plate.

--- src/components/public/StillPlate.tsx
+++ src/components/public/StillPlate.tsx
@@ -15,10 +15,16 @@ export function StillPlate({
   return (
     <figure className="m-0">
       <div
-        className={`relative w-full overflow-hidden bg-stone ${
-          bleed ? "max-h-[92svh]" : ""
-        }`}
-        style={{ aspectRatio: `${still.width} / ${still.height}` }}
+        className={
+          bleed
+            ? "relative h-[92svh] w-full overflow-hidden bg-stone"
+            : "relative w-full overflow-hidden bg-stone"
+        }
+        style={
+          bleed
+            ? undefined
+            : { aspectRatio: `${still.width} / ${still.height}` }
+        }
       >
         <Image
           src={still.src}
